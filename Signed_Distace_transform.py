8f01ad56bb519d273ef15a9d02f72ce78ad4ce2e
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2023-05-05 15:56:40 +0100

    Create Signed_Distace_transform.py

---
+++ Signed_Distace_transform.py
@@ -0,0 +1,20 @@
+import imageio
+import numpy as np
+import matplotlib.pyplot as plt
+import skfmm
+
+# Load image
+im = imageio.imread(r"C:\Users\Abbas Khan\Desktop\vae_codes\2D_VIZ\INP\1_IMG.png")
+ima = np.array(im)
+ima =ima[:,:,0]/255
+
+import skfmm
+
+def SDF(ima):
+    ima = np.stack((ima,ima,ima),2)
+    phi = np.int64(np.any(ima[:, :, :3], axis = 2))
+    phi = np.where(phi, 0, -1) + 0.5
+    sd = skfmm.distance(phi, dx = 1)
+    return sd
+
+a = SDF(ima)
